e7045348f804a7421a5e92f125da495a7bf44dfb
Dan Florio <dflo@danflorio.com>
2017-08-17 10:24:34 -0400

    add login

--- passport.js
+++ passport.js
@@ -89,4 +89,24 @@ module.exports = function(passport) {
             });
         });
     }));
+
+    passport.use('local-login', new LocalStrategy({
+        usernameField : 'email',
+        passwordField : 'password',
+        passReqToCallback : true
+    },
+    function(req, email, password, done) {
+        User.findOne({ 'email' : email }, function(err, user) {
+            if (err)
+                return done(err);
+
+            if (!user)
+                return done(null, false, req.flash('loginMessage', 'No user found.'));
+
+            if (!user.comparePassword(password))
+                return done(null, false, req.flash('loginMessage', 'Incorrect email or password.'));
+
+            return done(null, user);
+        })
+    }));
 }
