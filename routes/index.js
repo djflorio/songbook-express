ec15e873f1468d2b5277f259460a3a6e7fa3b04a
Dan Florio <dflo@danflorio.com>
2017-08-17 10:33:44 -0400

    add info to profile page

--- routes/index.js
+++ routes/index.js
@@ -45,8 +45,8 @@ function isLoggedIn(req, res, next) {
     if (req.isAuthenticated())
         return next();
 
-    // if they aren't redirect them to the home page
-    res.redirect('/');
+    // if they aren't redirect them to the login page
+    res.redirect('/login');
 }
 
 module.exports = router;
